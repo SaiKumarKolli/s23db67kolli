fa8bec954a6e70e6c765fa4860c8bfa97a65b63a
SaiKumarKolli <saikumarkolli005@gmail.com>
2023-04-14 00:00:54 -0500

    updating models

--- app.js
+++ app.js
@@ -4,12 +4,55 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var RecipesRouter = require('./routes/Recipes');
 var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
+var Recipes = require('./models/Recipes');
 
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+// We can seed the collection if needed on server start
+async function recreateDB(){
+ // Delete everything
+ await Recipes.deleteMany();
+ let instance1 = new Recipes({recipes_name:"fish fry",recipes_qunatity:"10",recipes_price:1500});
+ let instance2 = new Recipes({recipes_name:"palak paneer",recipes_qunatity:"15",recipes_price:1200});
+ let instance3 = new Recipes({recipes_name:"chicken tikka",recipes_qunatity:"20",recipes_price:1700});
+ instance1.save().then(()=>{
+    console.log("Object 1 created")
+  }).catch((err)=>{
+    console.log(err);
+  })
+  instance2.save().then(()=>{
+      console.log("Object 2 created")
+    }).catch((err)=>{
+      console.log(err);
+    })
+    instance3.save().then(()=>{
+        console.log("Object 3 created")
+      }).catch((err)=>{
+        console.log(err);
+      })
+}
+
+let reseed = true;
+if (reseed) {recreateDB();}
 var app = express();
 
 // view engine setup
@@ -22,11 +65,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Recipes', RecipesRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
