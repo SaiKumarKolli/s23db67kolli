73b5035231c3e9c0b3964b116b517c2bc7032086
SaiKumarKolli <saikumarkolli005@gmail.com>
2023-04-27 22:39:10 -0500

    simple changes

--- models/Recipes.js
+++ models/Recipes.js
@@ -1,8 +1,25 @@
+const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const RecipesSchema = mongoose.Schema({
     recipes_name: String,
     recipes_qunatity: String,
     recipes_price: Number
+    // recipes_name: {
+    //     type: String,
+    //     required: true
+    // },
+    // recipes_qunatity: {
+    //     type: Number,
+    //     required: true,
+    //     min: 0,
+    //     max: 20
+    // },
+    // Recipes_price: {
+    //     type: Number,
+    //     required: true,
+    //     min: 0,
+    //     max: 2000
+    // }
 })
 module.exports = mongoose.model("Recipes",
     RecipesSchema)
